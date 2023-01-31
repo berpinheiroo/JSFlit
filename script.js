0f65aefcd1708ccac4de40a57d8cbee0fcd0defb
Bernardo Pinheiro <111240182+berpinheiroo@users.noreply.github.com>
2023-01-31 17:35:15 -0300

    1.2

--- script.js
+++ script.js
@@ -4,3 +4,7 @@ division = 80 / 3
 rest = 80 % 3
 addition += 5
 addition -= 3
+
+// caminho_5km && corro_2km, passo_o_dia_bem
+// levanto_cedo || antes_das_8h, saio_tranquilo
+// !termino_o_trabalho || !tenho_compromisso, saio_tarde
