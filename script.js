fe80a168896317fd34139cd9abdb10490c7c07c8
Bernardo Pinheiro <111240182+berpinheiroo@users.noreply.github.com>
2023-01-31 17:39:39 -0300

    1.2

--- script.js
+++ script.js
@@ -5,6 +5,6 @@ rest = 80 % 3
 addition += 5
 addition -= 3
 
-// caminho_5km && corro_2km, passo_o_dia_bem
-// levanto_cedo || antes_das_8h, saio_tranquilo
-// !termino_o_trabalho || !tenho_compromisso, saio_tarde
+caminho_5km && corro_2km, passo_o_dia_bem
+levanto_cedo || antes_das_8h, saio_tranquilo
+!termino_o_trabalho || !tenho_compromisso, saio_tarde
